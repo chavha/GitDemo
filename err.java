61fcbf201e861cd9bb3ae923a57c245a3c16347d
manju <chavhan72@gmail.com>
2020-10-26 14:14:12 +0530

    first commit

---
+++ err.java
@@ -0,0 +1,8 @@
+class eer
+ 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ manju1.java
@@ -0,0 +1,23 @@
+class manju1 
+{
+	public static void main(String[] args) 
+	{
+     int per 80;
+	 if(per>=70)
+		{
+		 System.out.println("FCD");
+		}
+	 else if(per>=(70-10) && per<70)
+		{
+		 System.out.println("FC");
+		}
+	 else if(per>=(49+1) && per<(70-10))
+		{
+	     System.out.println("SC");
+		}
+	 else
+		{
+		 System.out.println("FAIL");
+		}
+	}
+}
